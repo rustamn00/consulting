6ae571b5fe9afe042914f693668171513ec379bc
rustamn00 <exellentgo3@gmail.com>
2023-02-07 21:31:27 +0500

    2023 updated 3

--- components/index.js
+++ components/index.js
@@ -1,8 +1,5 @@
-import Contacts from "./contacts/Contancts";
 import Features from "./features/Features";
 import Footer from "./footer/Footer";
-import Pricing from "./pricing/Pricing";
-import Products from "./products/Products";
 import Navbar from "./header/Navbar";
 
-export { Contacts, Features, Footer, Pricing, Products, Navbar };
+export { Features, Footer, Navbar };
